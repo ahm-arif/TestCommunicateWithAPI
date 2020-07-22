f49cf641e419d08c9a4b43ba133ee1da22d97bd4
Dion Saputra <diondev01@gmail.com>
2020-07-22 15:29:08 +0700

    Add bottom navigation bar

--- apps/lib/pages/home/home_page.dart
+++ apps/lib/pages/home/home_page.dart
@@ -9,59 +9,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  String avatarUrl =
+      "https://www.inpixio.com/remove-background/images/animation-light-square.gif";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Center(),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
   AppBar _buildAppBar() {
     return AppBar(
-      title: Text(
-        "Wellness Center",
-      ),
+      title: Text("Wellness Center"),
       backgroundColor: primaryColor,
-      actions: [
-        IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
-        IconButton(icon: Icon(Icons.message), onPressed: () {}),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 15,
-                backgroundImage: NetworkImage(
-                    "https://www.inpixio.com/remove-background/images/animation-light-square.gif"),
-              )
-            ],
+      actions: _buildAppBarActions(),
+    );
+  }
+
+  List<Widget> _buildAppBarActions() {
+    return [
+      IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+      IconButton(icon: Icon(Icons.message), onPressed: () {}),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 15,
+              backgroundImage: NetworkImage(avatarUrl),
+            )
+          ],
+        ),
+      )
+    ];
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: primaryColor,
+        primaryColor: Colors.white,
+        textTheme: Theme.of(context)
+            .textTheme
+            .copyWith(caption: TextStyle(color: Colors.black54)),
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Beranda'),
           ),
-        )
-      ],
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.help),
+            title: new Text('Konsultasi'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.date_range),
+            title: Text('Jadwal'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Akun'),
+          ),
+        ],
+      ),
     );
   }
 }
