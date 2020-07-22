5c5ef9e3393abbd2361962683c0c940c0978ff15
Priagung Satyagama <priagungsatyagama@gmail.com>
2020-07-22 10:56:07 +0700

    init

---
+++ src/main/java/com/wellnes/testcommunicate/TestCommunicateApplication.java
@@ -0,0 +1,13 @@
+package com.wellnes.testcommunicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommunicateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestCommunicateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/wellnes/testcommunicate/TestCommunicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellnes.testcommunicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCommunicateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
