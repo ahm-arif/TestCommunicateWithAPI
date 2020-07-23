eeb196e96e85d9ab59c028183335e5459871abcc
Dion Saputra <diondev01@gmail.com>
2020-07-23 13:21:47 +0700

    Move carousel to homepage

--- apps/lib/pages/consult_page.dart
+++ apps/lib/pages/consult_page.dart
@@ -2,18 +2,33 @@ import 'package:flutter/material.dart';
 
 class ConsultPage extends StatefulWidget {
   @override
-  _RedState createState() => _RedState();
+  _ConsultState createState() => _ConsultState();
 }
 
-class _RedState extends State<ConsultPage> {
+class _ConsultState extends State<ConsultPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text(
-          "Konsultasi",
-          style: TextStyle(fontSize: 20.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            _buildSearchInput(),
+          ],
         ),
+      ),
+    );
+  }
+
+  TextField _buildSearchInput() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Cari kategori keluhan",
+        prefixIcon: Icon(Icons.search),
+        labelStyle: TextStyle(fontSize: 12.0),
+        contentPadding: EdgeInsets.all(0.0),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
       ),
     );
   }
