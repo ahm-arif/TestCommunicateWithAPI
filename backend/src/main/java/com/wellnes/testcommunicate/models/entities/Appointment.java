6ea596723f941fa6ff82e2c72fff87575a1d9960
Priagung Satyagama <priagungsatyagama@gmail.com>
2020-07-25 16:54:11 +0700

    modified models

--- backend/src/main/java/com/wellnes/testcommunicate/models/entities/Appointment.java
+++ backend/src/main/java/com/wellnes/testcommunicate/models/entities/Appointment.java
@@ -5,24 +5,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Complaint {
+public class Appointment {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
+  private int appointmentId;
 
-  private String name;
+  @ManyToOne
+  @JoinColumn(name = "doctor_id")
+  private Doctor doctor;
 
-  private String description;
+  private int patientId;
+
+  @ManyToOne
+  @JoinColumn(name = "appointment_type_id")
+  private AppointmentType appointmentType;
+
+  private int prescriptionId;
+
+  @ManyToOne
+  @JoinColumn(name = "symptom_id")
+  private Symptom symptom;
 
 }
--- backend/src/main/java/com/wellnes/testcommunicate/models/entities/Doctor.java
+++ backend/src/main/java/com/wellnes/testcommunicate/models/entities/Doctor.java
@@ -5,10 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Builder
@@ -19,9 +17,31 @@ public class Doctor {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
+  private int doctorId;
 
   private String name;
 
-  private String specialization;
+  private String title;
+
+  private String gender;
+
+  private String DOB;
+
+  private String phone;
+
+  private String address;
+
+  private int appuserId;
+
+  @OneToMany(mappedBy = "doctor")
+  private List<Appointment> appointments;
+
+  @ManyToMany
+  @JoinTable(
+          name = "doctor_doctor_category",
+          joinColumns = @JoinColumn(name = "doctor_id"),
+          inverseJoinColumns = @JoinColumn(name = "doctor_category_id")
+  )
+  private List<DoctorCategory> doctorCategories;
+
 }
